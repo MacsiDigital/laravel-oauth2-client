b113776281fdb2d465dea54f9b6b2dbb02f92f89
Colin Hall <colin@macsi.co.uk>
2021-01-05 13:59:16 +0000

    Update tests

---
+++ tests/Feature/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MacsiDigital\OAuth2\Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function base_test()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MacsiDigital\OAuth2\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function base_test()
+    {
+        $this->assertTrue(true);
+    }
+}
